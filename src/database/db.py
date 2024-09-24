4e4e2d24a18bc6872c2484e34c7c7c33564c59a6
Valeriya <Dzhavadova.v@yandex.ru>
2024-09-24 14:09:32 +0300

    add Base

--- src/database/db.py
+++ src/database/db.py
@@ -1,6 +1,7 @@
 from typing import AsyncGenerator
 
 from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker, AsyncSession
+from sqlalchemy.orm import DeclarativeBase
 
 from src.config import settings
 
@@ -9,6 +10,10 @@ async_engine = create_async_engine(url=settings.db.URL.get_secret_value())
 async_session_maker = async_sessionmaker(
     bind=async_engine, class_=AsyncSession, expire_on_commit=False
 )
+
+
+class Base(DeclarativeBase):
+    pass
 
 
 async def get_async_session() -> AsyncGenerator[AsyncSession, None]:
