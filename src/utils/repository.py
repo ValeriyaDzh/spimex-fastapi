c00ac2885d6166c674d6d9099f3ec7e5c187162d
Valeriya <Dzhavadova.v@yandex.ru>
2024-09-26 12:52:36 +0300

    add get_orderly_query_with_limit

--- src/utils/repository.py
+++ src/utils/repository.py
@@ -1,12 +1,13 @@
-from uuid import UUID
 import logging
-from typing import TypeVar
+from typing import TYPE_CHECKING, TypeVar, Sequence, Any
 
-from sqlalchemy import select, and_
+from sqlalchemy import select, and_, desc, Column
 from sqlalchemy.ext.asyncio import AsyncSession
 
-
 from src.models import Base
+
+if TYPE_CHECKING:
+    from sqlalchemy.engine import Result
 
 T = TypeVar("T", bound=Base)
 
@@ -32,8 +33,19 @@ class SqlAlchemyRepository:
         self.session.add_all(entities)
         await self.session.commit()
 
-    async def get_by_id(self, entity_id: UUID) -> T:
-        result = await self.session.execute(
+    async def get_by_id(self, entity_id: Any) -> T:
+        result: Result = await self.session.execute(
             select(self.model).where(and_(self.model.id == entity_id))
         )
         return result.scalar_one_or_none()
+
+    async def get_by_query_all(self, **kwargs: Any) -> Sequence[T]:
+        query = select(self.model).filter_by(**kwargs)
+        result: Result = await self.session.execute(query)
+        return result.scalars().all()
+
+    async def get_orderly_query_with_limit(self, column: Column, limit: int) -> list[T]:
+        result: Result = await self.session.execute(
+            select(column).group_by(column).order_by(desc(column)).limit(limit)
+        )
+        return result.scalars().all()
