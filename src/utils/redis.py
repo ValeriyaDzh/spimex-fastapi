844404585941b9f241f7d3a33c36262494d6e987
Valeriya <Dzhavadova.v@yandex.ru>
2024-09-27 19:09:17 +0300

    create func init_redis_cache

--- src/utils/redis.py
+++ src/utils/redis.py
@@ -1,7 +1,13 @@
 from redis import asyncio as aioredis
+from fastapi_cache import FastAPICache
+from fastapi_cache.backends.redis import RedisBackend
 
 from src.config import settings
 
 redis = aioredis.from_url(
     f"redis://{settings.redis.HOST}", encoding="utf8", decode_responses=True
 )
+
+
+def init_redis_cache() -> None:
+    FastAPICache.init(RedisBackend(redis), prefix="fastapi-cache")
