200a003fbc17a91ec4440b22fcb43de7308215e9
Valeriya <Dzhavadova.v@yandex.ru>
2024-10-08 23:44:54 +0300

    add excel_file fixture

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,7 +1,9 @@
 import logging
 from typing import AsyncGenerator
+from io import BytesIO
 
 import pytest
+import pandas as pd
 from fastapi.testclient import TestClient
 from fastapi_cache import FastAPICache
 from fastapi_cache.backends.inmemory import InMemoryBackend
@@ -9,7 +11,7 @@ from httpx import AsyncClient
 from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine, async_sessionmaker
 from sqlalchemy.pool import NullPool
 
-from tests.fake_data import TEST_SPIMEX_DATA
+from tests.fake_data import TEST_SPIMEX_DATA, TEST_EXCEL_DATA
 from src.database import get_async_session
 from src.config import settings
 from src.main import app
@@ -67,10 +69,22 @@ async def prepare_db():
     logger.info("Database tables delete successfully.")
 
 
-@pytest.fixture(autouse=True, scope="function")
+@pytest.fixture(scope="function", autouse=True)
 def fastapi_cache():
     FastAPICache.init(InMemoryBackend())
     yield
+
+
+@pytest.fixture(scope="function", autouse=True)
+async def test_session():
+    async with async_session_maker() as session:
+        yield session
+
+
+@pytest.fixture
+def mock_session(mocker):
+    session = mocker.Mock(spec=AsyncSession)
+    return session
 
 
 client = TestClient(app)
@@ -80,3 +94,14 @@ client = TestClient(app)
 async def api_client() -> AsyncGenerator[AsyncClient, None]:
     async with AsyncClient(app=app, base_url="http://test") as api_client:
         yield api_client
+
+
+@pytest.fixture
+def excel_file():
+    df = pd.DataFrame(TEST_EXCEL_DATA)
+    print(df)
+    excel_buffer = BytesIO()
+    with pd.ExcelWriter(excel_buffer) as writer:
+        df.to_excel(writer, index=False, sheet_name="Sheet1")
+
+    return excel_buffer
